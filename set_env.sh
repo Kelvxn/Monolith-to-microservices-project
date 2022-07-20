25113d70c17617fca0ca7fd64d02f4e25f21d31b
Chibuike Akpulu <akpuluchibuike3@gmail.com>
2022-07-20 02:40:34 +0100

    Delete set_env.sh

--- set_env.sh
+++
@@ -1,9 +0,0 @@
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=udagram-db.cz1cjkpxyb0r.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-160632968356-dev
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
